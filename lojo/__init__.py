0333ba49584cd8d35096293025071208a2cbe23b
ericgebers <ericgebers@gmail.com>
2014-03-01 12:49:21 -0700

    Remove postgresql changes

--- lojo/__init__.py
+++ lojo/__init__.py
@@ -1,6 +1,3 @@
-import os
-import psycopg2
-import urlparse
 from pyramid.config import Configurator
 
 
@@ -13,15 +10,3 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
-
-    #Postgresql Database
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-)
