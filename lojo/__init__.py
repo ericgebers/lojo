4b8caf9768a4470c0badfe0691f27d72ee460f77
ericgebers <ericgebers@gmail.com>
2014-03-01 12:21:26 -0700

    Add Postgresql DB

--- lojo/__init__.py
+++ lojo/__init__.py
@@ -1,3 +1,6 @@
+import os
+import psycopg2
+import urlparse
 from pyramid.config import Configurator
 
 
@@ -10,3 +13,15 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
+
+    #Postgresql Database
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+)
