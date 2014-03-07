43adabb28cc21cfefa210a60cbb7c7601c7a82c1
ericgebers <ericgebers@gmail.com>
2014-03-06 21:29:16 -0700

    revert test

--- lojo/views.py
+++ lojo/views.py
@@ -4,8 +4,3 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'lojo'}
-
-
-@view_config(route_name='test', renderer='templates/mytemplate.pt')
-def my_test(request):
-    return{'project': 'lojo'}
