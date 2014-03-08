71b24e0d9a791c652c267486a52323c3e08caf19
ericgebers <ericgebers@gmail.com>
2014-03-08 12:07:44 -0700

    add about

--- lojo/views.py
+++ lojo/views.py
@@ -4,3 +4,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'lojo'}
+
+
+@view_config(route_name='about', renderer='templates/mytemplate.pt')
+def my_view2(request):
+    return {'project': 'lojo'}
